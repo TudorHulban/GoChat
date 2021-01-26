f41d1c1ecf97bfcb2f97a288248e7ff7b6f10a8a
Tudor Hulban <prindex2010@gmail.com>
2021-01-26 13:14:45 +0200

    reverted to fiber example

--- go.mod
+++ go.mod
@@ -4,6 +4,5 @@ go 1.15
 
 require (
 	github.com/gofiber/fiber/v2 v2.3.3
-	github.com/gofiber/template v1.6.6
 	github.com/gofiber/websocket/v2 v2.0.2
 )
--- main.go
+++ main.go
@@ -1,37 +1,94 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/template/html"
+	"github.com/gofiber/websocket/v2"
 )
 
-func main() {
-	// Create a new engine
-	engine := html.New("./views", ".gohtml")
+type client struct{} // Add more data to this type if needed
 
-	// Pass the engine to the Views
-	app := fiber.New(fiber.Config{
-		Views: engine,
-	})
+var clients = make(map[*websocket.Conn]client) // Note: although large maps with pointer-like types (e.g. strings) as keys are slow, using pointers themselves as keys is acceptable and fast
+var register = make(chan *websocket.Conn)
+var broadcast = make(chan string)
+var unregister = make(chan *websocket.Conn)
 
-	app.Get("/", handler)
-	app.Get("/p", handlerUser)
+func runHub() {
+	for {
+		select {
+		case connection := <-register:
+			clients[connection] = client{}
+			log.Println("connection registered")
 
-	log.Fatal(app.Listen(":3000"))
-}
+		case message := <-broadcast:
+			log.Println("message received:", message)
 
-func handler(c *fiber.Ctx) error {
-	return c.Render("index", fiber.Map{
-		"Person": "John",
-	})
-}
+			// Send the message to all clients
+			for connection := range clients {
+				if err := connection.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
+					log.Println("write error:", err)
 
-func handlerUser(c *fiber.Ctx) error {
-	u := User{
-		Name: "John",
+					unregister <- connection
+					connection.WriteMessage(websocket.CloseMessage, []byte{})
+					connection.Close()
+				}
+			}
+
+		case connection := <-unregister:
+			// Remove the client from the hub
+			delete(clients, connection)
+
+			log.Println("connection unregistered")
+		}
 	}
+}
 
-	return c.Render("index", u)
+func main() {
+	app := fiber.New()
+
+	app.Static("/", "./home.html")
+
+	app.Use(func(c *fiber.Ctx) error {
+		if websocket.IsWebSocketUpgrade(c) { // Returns true if the client requested upgrade to the WebSocket protocol
+			return c.Next()
+		}
+		return c.SendStatus(fiber.StatusUpgradeRequired)
+	})
+
+	go runHub()
+
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		// When the function returns, unregister the client and close the connection
+		defer func() {
+			unregister <- c
+			c.Close()
+		}()
+
+		// Register the client
+		register <- c
+
+		for {
+			messageType, message, err := c.ReadMessage()
+			if err != nil {
+				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+					log.Println("read error:", err)
+				}
+
+				return // Calls the deferred function, i.e. closes the connection on error
+			}
+
+			if messageType == websocket.TextMessage {
+				// Broadcast the received message
+				broadcast <- string(message)
+			} else {
+				log.Println("websocket message received of type", messageType)
+			}
+		}
+	}))
+
+	addr := flag.String("addr", ":8080", "http service address")
+	flag.Parse()
+	log.Fatal(app.Listen(*addr))
 }
